9ff89307c9542005f06f9073e2f268774ec84b43
Ubuntu <201201886@csecnu.v52bed5ic50ejeplw3mbfofysf.mx.internal.cloudapp.net>
2016-04-08 08:57:50 +0000

    fix

--- dc05/packet_capture.py
+++ dc05/packet_capture.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import sys
 
 class EthernetFrame(object):
     def __init__(self, buf):
@@ -9,9 +10,7 @@ class EthernetFrame(object):
         self.srcMac = ':'.join([packet[1].encode('hex')[x:x+2] for x in range(0, len(packet[1].encode('hex')), 2)])
         self.frameType = '0x'+packet[2].encode('hex')
         self.nextFrame = None
-        print type(self.frameType)
-        if self.frameType is '0x0800':
-            print 'test'
+        if self.frameType == '0x0800':
             self.nextFrame = IPv4Header(self.nextData)
    
     def dump(self):
@@ -21,31 +20,30 @@ class EthernetFrame(object):
         print "Destination MAC Address : ", self.dstMac
         print "Source MAC Address : ", self.srcMac
         print "Type : ", self.frameType
-        #print type(self.nextFrame)
+        
         if self.nextFrame is not None:
             self.nextFrame.dump()
 
 class IPv4Header(object):
     def __init__(self, buf):
-        packet = struct.unpack("!2s2s2s2s2s2s4s4s", buf[0][0:20])
+        packet = struct.unpack("!2s2s2s2s2s2s4s4s", buf[0:20])
+        self.totalLength = int(packet[1].encode('hex'), 16)
         self.sourceIp = socket.inet_ntoa(packet[6])
-        self.destIp = socket.inet_nota(packet[7])
+        self.destIp = socket.inet_ntoa(packet[7])
    
     def dump(self):
         print "==========================="
-        print "IPv4"
+        print "IPv4 Header"
         print "==========================="
-        print "Source IP Address : ", self.sourceIP
-        print "Destination IP Address : ", self.destIP       
+        print "Toal Length : ", self.totalLength
+        print "Source IP Address : ", self.sourceIp
+        print "Destination IP Address : ", self.destIp       
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
     #print 'test'
     while True:
         packet = sock.recvfrom(4096)
+        print 'packet: ', sys.getsizeof(packet)
         eth_header = EthernetFrame(packet)
         eth_header.dump()
-        #data = struct.unpack("!4s4s4s4s4s", packet[0][14:34])
-        #src_ip = socket.inet_ntoa(data[3])
-        #dst_ip = socket.inet_ntoa(data[4])
-        #print 'protocol_type: ', protocol_type, 'src_ip: ', src_ip, 'dst_ip: ', dst_ip
